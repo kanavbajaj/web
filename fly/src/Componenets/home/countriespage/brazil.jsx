4f0633e713b3f40a251764406a73000d5ebd9377
jaineel sampat <142526912+JaineelSampat@users.noreply.github.com>
2024-03-28 01:00:44 +0530

    Update and rename brazil.html to brazil.jsx

---
+++ fly/src/Componenets/home/countriespage/brazil.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+
+function BrazilInformation() {
+  return (
+    <div>
+      <h2 style={{textAlign: 'center', fontFamily: '"Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande", "Lucida Sans", Arial, sans-serif'}}>
+        BRAZIL
+      </h2>
+      <div style={{
+        maxWidth: '800px',
+        margin: '20px auto',
+        color: 'dimgray',
+        padding: '20px',
+        border: '1px solid burlywood',
+        overflow: 'hidden'
+      }}>
+        <div style={{float: 'left', margin: '0 15px 10px 0'}}>
+          <img
+            src="C:\\Users\\jaine\\OneDrive\\Desktop\\brazil.jpeg"
+            alt="Brazil"
+            width="200"
+          />
+        </div>
+        <p>
+          Brazil is the largest country in South America, spanning over half of
+          the continent's land area. It shares borders with every South American
+          country except Chile and Ecuador. Brazil's diverse geography includes
+          the Amazon Rainforest, the world's largest tropical rainforest, which
+          covers a significant portion of the country's northern region. The
+          Amazon River, the world's largest river by discharge volume, flows
+          through Brazil. The country also features other geographical landmarks
+          such as the Pantanal, the world's largest tropical wetland area, and
+          the Atlantic Forest, a biodiversity hotspot. Brazil has a long coastline
+          along the Atlantic Ocean, with popular beach destinations like Rio de
+          Janeiro and Salvador.
+        </p>
+        <div style={{float: 'right', margin: '0 0 20px 20px'}}>
+          <img
+            src="C:\\Users\\jaine\\OneDrive\\Desktop\\brazil music.jpeg"
+            alt="djembe"
+            width="200"
+          />
+        </div>
+        <p>
+          Brazilian culture is a vibrant blend of indigenous, African, European,
+          and Asian influences, reflecting the country's diverse heritage. Brazil
+          is renowned for its music and dance, with genres like samba, bossa nova,
+          and forró captivating audiences worldwide. The annual Carnival
+          celebration, held before Lent, is one of the largest and most famous
+          festivals in the world, featuring colorful parades, elaborate costumes,
+          and lively street parties. Brazilian cuisine is rich and varied, with
+          dishes like feijoada (a hearty bean stew), churrasco (barbecue), and
+          brigadeiro (chocolate truffles) being popular favorites. The country's
+          cultural diversity is also evident in its art, literature, and religious
+          practices, with traditions ranging from Capoeira (a martial art with
+          elements of dance and music) to Candomblé and Umbanda (Afro-Brazilian
+          religions).
+        </p>
+        <div style={{float: 'left', margin: '0 15px 10px 0'}}>
+          <img
+            src="C:\\Users\\jaine\\OneDrive\\Desktop\\brazil museum of tomo.jpeg"
+            alt="museum of Tomorrow"
+            width="200"
+          />
+        </div>
+        <p>
+          Brazilian architecture encompasses a wide range of styles influenced by
+          different historical periods and cultural traditions. Colonial
+          architecture, characterized by Portuguese influence, can be found in
+          cities like Salvador and Olinda, with well-preserved historic districts
+          featuring colorful colonial-era buildings, cobblestone streets, and
+          baroque churches. In modern Brazil, cities like Brasília, the capital,
+          showcase innovative architecture and urban planning, with landmark
+          buildings designed by renowned architects like Oscar Niemeyer. Brazil is
+          also known for its contemporary architectural landmarks, such as the
+          Museum of Tomorrow in Rio de Janeiro and the São Paulo Museum of Art
+          (MASP), which highlight the country's creative and architectural prowess
+          on the global stage.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default BrazilInformation;
+
